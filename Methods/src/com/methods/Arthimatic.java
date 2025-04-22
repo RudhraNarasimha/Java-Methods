ddffe3e016375f06a07a3c037e59b1ed76b940ac
user <user@DESKTOP-OC2FCPH>
2025-04-22 11:38:14 +0530

    Methods

---
+++ Methods/src/com/methods/Arthimatic.java
@@ -0,0 +1,51 @@
+package com.methods;
+
+import java.util.Scanner;
+
+public class Arthimatic {
+
+	int a, b, c;
+	Scanner scanner = new Scanner(System.in);
+
+// without returntype and without parameters
+	void add() {
+		System.out.println("Enter A Value : ");
+		a = scanner.nextInt();
+		System.out.println("Enter A Value : ");
+		b = scanner.nextInt();
+		c = a + b;
+		System.out.println("Addition is : " + c);
+
+	}
+
+	// without returntype and with parameters
+	void sub(int x, int y) {
+		a = x;
+		b = y;
+		c = a - b;
+
+		System.out.println("Difference is : " + c);
+
+	}
+
+	int mul() {
+		System.out.println("Enter A Value : ");
+		a = scanner.nextInt();
+		System.out.println("Enter A Value : ");
+		b = scanner.nextInt();
+		c = a * b;
+		return c;
+
+	}
+
+	int div(int x, int y) {
+
+		a = x;
+
+		b = y;
+
+		c = a / b;
+
+		return c;
+	}
+}
