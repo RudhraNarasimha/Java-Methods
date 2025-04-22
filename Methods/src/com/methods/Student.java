9fbdb14bbec96a8e4a14867a9c2b6a98294428ba
user <user@DESKTOP-OC2FCPH>
2025-04-22 11:57:40 +0530

    Methods

---
+++ Methods/src/com/methods/Student.java
@@ -0,0 +1,15 @@
+package com.methods;
+
+public class Student {
+
+	public static String getFullName(String fname, String lname) {
+
+		String fullname = fname + "  " + lname;
+
+		return fullname;
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Methods/src/com/methods/StudentTest.java
@@ -0,0 +1,13 @@
+package com.methods;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String fullname = Student.getFullName("Rudhra", "Narasimha");
+		System.out.println(fullname);
+
+	}
+
+}
